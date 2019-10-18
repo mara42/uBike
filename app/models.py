0d6d12dcd659f8c615cab1065b63b151606f9c73
Veluga <albertboehm@hotmail.de>
2019-10-18 19:14:35 +0100

    Added Card model.

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 class Bike(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rentals = db.relationship('Rental', backref='Bike', lazy=True)
+    location = db.Column(db.String)
 
     def __repr__(self):
         return '<Bike {}>'.format(self.id)
@@ -10,6 +11,7 @@ class Bike(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rentals = db.relationship('Rental', backref='Client', lazy=True)
+    card_id = db.Column(db.Integer, db.ForeignKey('card.id'))
 
     def __repr__(self):
         return '<User {}>'.format(self.id)
@@ -18,6 +20,14 @@ class Rental(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     bike_id = db.Column(db.Integer, db.ForeignKey('bike.id'))
     client_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    return_date = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<Rental {}>'.format(self.id)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.relationship('User', backref='Client', lazy=True)
+
+    def __repr__(self):
+        return '<Card {}>'.format(self)
