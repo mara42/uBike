61c93fb37cdbbf1f8a55efa6cec5caa10c339bbb
Martti <martti@aukia.com>
2019-10-18 15:52:02 +0100

    fix routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+def hello():
+    return render_template('index.html', title='uBike')
